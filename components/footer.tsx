99a802c7014e3626442fb9e4f66676c71c2eefc6
kouya17 <17383696+kouya17@users.noreply.github.com>
2023-01-08 14:21:59 +0900

    feat: add company info

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,13 +3,13 @@ import { FaTwitter, FaGithub, FaLink } from 'react-icons/fa';
 
 const Footer: FunctionComponent = ({}) => (
     <div>
-        <div className="flex justify-center">
+        <div className="flex justify-center mt-10">
             <a href="https://twitter.com/aoki_kouya"><FaTwitter size={32} color="gray" className="mx-2"/></a>
             <a href="https://github.com/kouya17"><FaGithub size={32} color="gray" className="mx-2"/></a>
             <a href="https://kouya17.com"><FaLink size={32} color="gray" className="mx-2"/></a>
         </div>
         <div className="pt-3 pb-6 px-4 w-100 text-center">
-            <span className="text-sm text-gray-500 dark:text-gray-300">&copy; 2022 清明ソフト開発</span>
+            <span className="text-sm text-gray-500 dark:text-gray-300">&copy; 2023 清明ソフト開発</span>
         </div>
     </div>
 );
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 import Footer from '../components/footer'
-import Navigation from '../components/navigation'
 
 const Home: NextPage = () => {
   return (
@@ -16,33 +15,70 @@ const Home: NextPage = () => {
         <meta name="twitter:card" content="summary" />
       </Head>
       <main className='flex-grow container max-w-lg mx-auto px-4 flex flex-col min-h-screen'>
-        <h1 className='text-3xl font-bold py-10 text-center'>
+        <h1 className='text-4xl font-bold py-10 text-center'>
           清明ソフト開発
         </h1>
 
-        <div className='py-3'>
-          小規模なソフト開発を承ります。主に動画処理、Webスクレイピング周りを強みとしています。
-        </div>
+        <h2 className='text-xl pb-3 text-center font-bold'>サービス内容</h2>
+        <ul className='list-disc py-2'>
+          <li>Webスクレイピングソフトの開発</li>
+          <li>動画配信システム構築コンサルタント</li>
+          <li>Webアプリの開発</li>
+          <li>各種ツールの開発</li>
+          <li>電子工作教育</li>
+        </ul>
         <a href="https://forms.gle/TEoSozXFawU8DhbE8">
           <button className='my-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-full'>
             お問い合わせ
           </button>
         </a>
 
-        <h2 className='text-xl pt-8 pb-3 text-center'>実績例</h2>
-        <ul>
+        <h2 className='text-xl pt-8 pb-3 text-center font-bold'>実績例</h2>
+        <ul className='list-disc py-2'>
           <li>多段SSH用のスクリプト作成: 対価1万円程度</li>
           <li>画像ファイルからタイムラプスを生成するWebアプリ: 対価5万円程度</li>
           <li>データベースソフト「Fullfree」向けのデータベース設計: 対価3万円程度</li>
         </ul>
 
-        <h2 className='text-xl pt-8 pb-3 text-center'>清明ソフト開発について</h2>
-        <div>清明ソフト開発は神奈川県で活動しているフリーランスエンジニアの個人事務所です。私の経歴は以下から確認できます。</div>
-        <Link href="https://resume.kouya17.com/">
-          <a className='text-blue-600'>
-            https://resume.kouya17.com/
-          </a>
-        </Link>
+        <h2 className='text-xl pt-8 pb-3 text-center font-bold'>清明ソフト開発について</h2>
+        <div className='py-2'>
+          <div>清明ソフト開発は神奈川県で活動しているフリーランスエンジニアの個人事務所です。私の経歴は以下から確認できます。</div>
+          <Link href="https://resume.kouya17.com/">
+            <a className='text-blue-600'>
+              https://resume.kouya17.com/
+            </a>
+          </Link>
+        </div>
+        
+        <h2 className='text-xl pt-8 pb-3 text-center font-bold'>事業者概要</h2>
+        <table className='table-auto my-2'>
+          <tbody>
+            <tr className='border-b'>
+              <td>屋号</td>
+              <td>清明ソフト開発</td>
+            </tr>
+            <tr className='border-b'>
+              <td>屋号(カナ)</td>
+              <td>セイメイソフトカイハツ</td>
+            </tr>
+            <tr className='border-b'>
+              <td>連絡先</td>
+              <td><Link href="https://forms.gle/TEoSozXFawU8DhbE8"><a className='text-blue-600'>お問い合わせ</a></Link></td>
+            </tr>
+            <tr className='border-b'>
+              <td>設立年</td>
+              <td>2023年</td>
+            </tr>
+            <tr className='border-b'>
+              <td>事業内容</td>
+              <td>ITサービス・ソフトウェア開発</td>
+            </tr>
+            <tr className='border-b'>
+              <td>主要取引銀行</td>
+              <td>楽天銀行</td>
+            </tr>
+          </tbody>
+        </table>
       </main>
 
       <Footer />
