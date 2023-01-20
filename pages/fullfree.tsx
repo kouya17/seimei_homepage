d3e95375747194aa5be132687bb67c0622f83b03
kouya17 <17383696+kouya17@users.noreply.github.com>
2023-01-21 01:13:47 +0900

    fix: style

--- pages/fullfree.tsx
+++ pages/fullfree.tsx
@@ -1,9 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Footer from '../components/footer'
-import Image from 'next/image'
+import Header from '../components/header'
+import Link from 'next/link'
+import { FaTwitter, FaLine } from 'react-icons/fa';
+import { FiMail } from 'react-icons/fi'
 
 const Fullfree: NextPage = () => {
+  const navs = [
+    {text: "価格例", href: "#price"},
+    {text: "実績例", href: "#experience"},
+    {text: "お問い合わせ", href: "#contact"},
+  ]
+
   return (
     <div className='dark:bg-zinc-800 dark:text-gray-200'>
       <Head>
@@ -14,12 +23,17 @@ const Fullfree: NextPage = () => {
         <meta property="og:description" content="Fullfreeのカスタマイズは清明ソフト開発にお任せください。" />
         <meta name="twitter:card" content="summary" />
       </Head>
+      
+      <div className='bg-white dark:bg-zinc-900 sticky top-0'>
+        <Header navs={navs}/>
+      </div>
+      
       <main className='flex-grow container max-w-lg mx-auto px-4 flex flex-col'>
         <h1 className='text-4xl font-bold py-10 text-center'>
           Fullfreeのカスタマイズは清明ソフト開発にお任せください
         </h1>
 
-        <h2 className='text-xl py-3 text-center font-bold'>顧客管理コストの削減をご検討ですか？</h2>
+        <h2 className='text-2xl py-3 text-center font-bold'>顧客管理コストの削減をご検討ですか？</h2>
         <div className='py-2'>
           Fullfreeとは株式会社フリースタイルが開発している、無料で利用可能な顧客管理ソフトです。カスタマイズ性や操作性に優れ、顧客管理コストの削減に寄与します。
         </div>
@@ -27,7 +41,7 @@ const Fullfree: NextPage = () => {
           清明ソフト開発には国家資格である「データベーススペシャリスト」を所有している技術者が在籍しています。専門知識を駆使し、既存システムからの移行や、データベースシステムの新規開発を請け負います。
         </div>
 
-        <h2 className='text-xl pt-8 pb-3 text-center font-bold'>価格例</h2>
+        <h2 id="price" className='text-2xl pt-8 pb-3 text-center font-bold'>価格例</h2>
         <ul className='list-disc py-2 px-5'>
           <li>シンプルなデータ構成の場合: 5000円～</li>
           <li>関数の利用が必要なデータ構成の場合: 20000円～</li>
@@ -36,21 +50,33 @@ const Fullfree: NextPage = () => {
           そもそもFullfreeの利用が適切かどうかを含めたシステム構築に関するご相談等も承りますので、なんでもお気軽にお問い合わせください。
         </div>
 
-        <a href="https://forms.gle/TEoSozXFawU8DhbE8">
-          <button className='mt-2 mb-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-full'>
-            見積もりを依頼する
-          </button>
-        </a>
-        <div className='pt-3 pb-3'>LINEからの問い合わせも受け付けています。</div>
-        <div className='block mx-auto'>
-          <Image src="qr.png" alt="LINEアカウント友達登録用QRコード" width={150} height={150}/>
-        </div>
-
-        <h2 className='text-xl pt-8 pb-3 text-center font-bold'>実績例</h2>
+        <h2 id="experience" className='text-2xl pt-8 pb-3 text-center font-bold'>実績例</h2>
         <ul className='list-disc py-2 px-5'>
           <li>個人運営サービス向けの顧客管理データベース設計: 対価3万円程度</li>
           <li>法人運営サービス向けの顧客管理データベース検討: 対価2万円程度</li>
         </ul>
+        
+        <h2 id="contact" className='text-2xl pt-8 pb-3 text-center font-bold'>お問い合わせ</h2>
+        <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+          <Link href="mailto:seimei.soft.dev&#64;gmail.com">
+            <a>
+              <FiMail size={40} color="gray" />
+              <span className="sr-only">Mail address</span>
+            </a>
+          </Link>
+          <Link href="https://lin.ee/skBfy9m">
+            <a>
+              <FaLine size={40} color="#06C755" />
+              <span className="sr-only">Line account</span>
+            </a>
+          </Link>
+          <Link href="https://twitter.com/aoki_kouya">
+            <a>
+              <FaTwitter size={40} color="#1DA1F2" />
+              <span className="sr-only">Twitter account</span>
+            </a>
+          </Link>
+        </div>
       </main>
 
       <div className='mt-10 pt-8 pb-6 bg-gray-100 dark:bg-zinc-900'>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,19 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 import Footer from '../components/footer'
-import Image from 'next/image'
+import Header from '../components/header'
+import { FaTwitter, FaLine } from 'react-icons/fa';
+import { FiMail } from 'react-icons/fi'
 
 const Home: NextPage = () => {
+  const navs = [
+    { text: "ご挨拶", href: "#greet" },
+    { text: "実績例", href: "#experience" },
+    { text: "事業者概要", href: "#abstract" },
+    { text: "お問い合わせ", href: "#contact" },
+    { text: "リンク", href: "#link" },
+  ]
+
   return (
     <div className='dark:bg-zinc-800 dark:text-gray-200'>
       <Head>
@@ -15,58 +25,32 @@ const Home: NextPage = () => {
         <meta property="og:description" content="清明ソフト開発のTOPページ" />
         <meta name="twitter:card" content="summary" />
       </Head>
+
+      <div className='bg-white dark:bg-zinc-900 sticky top-0'>
+        <Header navs={navs} />
+      </div>
+
       <main className='flex-grow container max-w-lg mx-auto px-4 flex flex-col'>
+
         <h1 className='text-4xl font-bold py-10 text-center'>
           清明ソフト開発
         </h1>
 
-        <h2 className='text-xl pb-3 text-center font-bold'>サービス内容</h2>
-        <ul className='list-disc py-2 px-5'>
-          <li>Webスクレイピングソフトの開発</li>
-          <li>動画配信システム構築のコンサルタント</li>
-          <li>Webアプリの開発</li>
-          <li>各種ツールの開発</li>
-          <li>プログラミング教育</li>
-          <li>電子工作教育</li>
-        </ul>
-        <a href="https://forms.gle/TEoSozXFawU8DhbE8">
-          <button className='mt-2 mb-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-full'>
-            見積もりを依頼する
-          </button>
-        </a>
-        <div className='pt-3 pb-3'>LINEからの問い合わせも受け付けています。</div>
-        <div className='block mx-auto'>
-          <Image src="qr.png" alt="LINEアカウント友達登録用QRコード" width={150} height={150}/>
+        <h2 id="greet" className='text-2xl pb-3 text-center font-bold'>あなたに寄り添うソフト開発を</h2>
+        <div className='py-2'>
+          清明ソフト開発のホームページにご訪問いただき、ありがとうございます。私たちは、ソフトウェアを活用し、あなたのお困りごとの解消をお手伝いいたします。
+        </div>
+        <div className='py-2'>
+          組み込み機器の開発からウェブサービスの開発まで、ソフトウェアの知識が必要になる領域は多岐に渡ります。清明ソフト開発はソフトウェア及びその周辺を含めた全体最適を追及したサービスの提供を進めてまいります。
         </div>
 
-        <h2 className='text-xl pt-8 pb-3 text-center font-bold'>実績例</h2>
+        <h2 id="experience" className='text-2xl pt-8 pb-3 text-center font-bold'>実績例</h2>
         <ul className='list-disc py-2 px-5'>
           <li>多段SSH用のスクリプト作成: 対価1万円程度</li>
           <li>画像ファイルからタイムラプスを生成するWebアプリ: 対価5万円程度</li>
           <li>データベースソフト「Fullfree」向けのデータベース設計: 対価3万円程度</li>
         </ul>
-
-        <h2 className='text-xl pt-8 pb-3 text-center font-bold'>清明ソフト開発について</h2>
-        <div className='py-2'>
-          <div>清明ソフト開発は神奈川県で活動しているフリーランスエンジニアの個人事務所です。私の経歴は以下から確認できます。</div>
-          <Link href="https://resume.kouya17.com/">
-            <a className='text-blue-500 hover:underline'>
-              https://resume.kouya17.com/
-            </a>
-          </Link>
-        </div>
-        
-        <h2 className='text-xl pt-8 pb-3 text-center font-bold'>各種クラウドソーシングサービスのリンク</h2>
-        <div className='py-2'>
-          <div>以下サービス経由で依頼を出していただくことも可能です。</div>
-          <ul className='list-disc pt-1 px-5'>
-            <li><Link href="https://crowdworks.jp/public/employees/4608762"><a className='text-blue-500 hover:underline'>クラウドワークス</a></Link></li>
-            <li><Link href="https://www.lancers.jp/profile/kouya_17"><a className='text-blue-500 hover:underline'>ランサーズ</a></Link></li>
-            <li><Link href="https://coconala.com/users/2928527"><a className='text-blue-500 hover:underline'>ココナラ</a></Link></li>
-          </ul>
-        </div>
-        
-        <h2 className='text-xl pt-8 pb-3 text-center font-bold'>事業者概要</h2>
+        <h2 id="abstract" className='text-2xl pt-8 pb-3 text-center font-bold'>事業者概要</h2>
         <table className='table-auto my-2'>
           <tbody>
             <tr className='border-b'>
@@ -76,10 +60,6 @@ const Home: NextPage = () => {
             <tr className='border-b'>
               <td>代表者</td>
               <td>大平 航也</td>
-            </tr>
-            <tr className='border-b'>
-              <td>連絡先</td>
-              <td><Link href="https://forms.gle/TEoSozXFawU8DhbE8"><a className='text-blue-500 hover:underline'>お問い合わせ</a></Link></td>
             </tr>
             <tr className='border-b'>
               <td>開業日</td>
@@ -95,11 +75,44 @@ const Home: NextPage = () => {
             </tr>
           </tbody>
         </table>
+
+        <h2 id="contact" className='text-2xl pt-8 pb-3 text-center font-bold'>お問い合わせ</h2>
+        <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+          <Link href="mailto:seimei.soft.dev&#64;gmail.com">
+            <a>
+              <FiMail size={40} color="gray" />
+              <span className="sr-only">Mail address</span>
+            </a>
+          </Link>
+          <Link href="https://lin.ee/skBfy9m">
+            <a>
+              <FaLine size={40} color="#06C755" />
+              <span className="sr-only">Line account</span>
+            </a>
+          </Link>
+          <Link href="https://twitter.com/aoki_kouya">
+            <a>
+              <FaTwitter size={40} color="#1DA1F2" />
+              <span className="sr-only">Twitter account</span>
+            </a>
+          </Link>
+        </div>
+
+        <h2 id="link" className='text-2xl pt-8 pb-3 text-center font-bold'>各種クラウドソーシングサービスのリンク</h2>
+        <div className='py-2'>
+          <div>以下サービス経由で依頼を出していただくことも可能です。</div>
+          <ul className='list-disc pt-1 px-5'>
+            <li><Link href="https://crowdworks.jp/public/employees/4608762"><a className='text-blue-500 hover:underline'>クラウドワークス</a></Link></li>
+            <li><Link href="https://www.lancers.jp/profile/kouya_17"><a className='text-blue-500 hover:underline'>ランサーズ</a></Link></li>
+            <li><Link href="https://coconala.com/users/2928527"><a className='text-blue-500 hover:underline'>ココナラ</a></Link></li>
+          </ul>
+        </div>
+
       </main>
 
       <div className='mt-10 pt-8 pb-6 bg-gray-100 dark:bg-zinc-900'>
         <div className='flex-grow container max-w-lg mx-auto px-4 flex flex-col'>
-          <Footer/>
+          <Footer />
         </div>
       </div>
 
